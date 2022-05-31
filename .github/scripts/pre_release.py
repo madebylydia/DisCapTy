9dc643d8bfe0dc0979b2e2c154f6a7aba4a2c95c
Predeactor <pro.julien.mauroy@gmail.com>
2022-06-01 01:53:51 +0200

    Attempt to fix pre release

--- .github/scripts/pre_release.py
+++ .github/scripts/pre_release.py
@@ -29,11 +29,11 @@ def check_pypi_version(local_version: str):
 
 
 def check_local_version_against_pyproject(discapty_version: str):
-    pyproject_file = os.path.join(
-        os.path.abspath(__file__), "..", "..", "pyproject.toml"
+    pyproject_file = os.path.abspath(
+        os.path.join(__file__, "..", "..", "pyproject.toml")
     )
 
-    with open(pyproject_file, "r") as f:
+    with open(pyproject_file, "") as f:
         pyproject_version = REG.search(f.read())
 
         if not pyproject_version:
