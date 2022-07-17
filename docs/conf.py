a18e162022f64c7ce801cb0c4ef61576b03c2ece
Julien Mauroy <61093863+Predeactor@users.noreply.github.com>
2022-07-17 12:53:17 +0200

    docs: enable intersphinx extension

--- docs/conf.py
+++ docs/conf.py
@@ -33,7 +33,7 @@ version = release
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = ["sphinx.ext.napoleon", "sphinx.ext.autodoc", "sphinx_copybutton"]
+extensions = ["sphinx.ext.napoleon", "sphinx.ext.autodoc", "sphinx.ext.intersphinx", "sphinx_copybutton"]
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ["_templates"]
